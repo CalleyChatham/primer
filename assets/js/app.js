7580aba29b134e37749af19811e9e973f613ebdf
Prasanth Nannapaneni <pnannapa@nm.org>
2017-03-29 15:53:48 -0500

    href for each event; details about event; created local files for js & css files

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,98 @@ console.log('App is running');
 $(document).ready(function() {
     //sS23z3239cvjhmN3 - key for eventful
 
+    var appKey = "sS23z3239cvjhmN3"; //prasanth's app key for eventful
+
+    function goToResults(){
+        $('body').attr('id','results');
+    }
+
+    $( function() {
+      $( "#start-date" ).datepicker();
+    });
+
+    $( function() {
+      $( "#end-date" ).datepicker();
+    });
+
+
+    function getEventDetails(eventId){
+        console.log(eventId);
+        $('body').attr('id','full');
+        $('#event-details').empty();
+        $('#event-image').empty();
+
+        //need this to call API and get event information
+        var eArgs = {
+            app_key: appKey,
+            id: eventId
+        };
+        EVDB.API.call("/events/get", eArgs, function(oData) {
+            var eventinfo = oData;
+            var eventTitle = '<div>' + eventinfo.title +'<div>';
+            var eventAddress = '<div>' + eventinfo.address +'<div>';
+            var eventVenue = '<div>' + eventinfo.venue_name +'<div>';
+            var eventCity = '<div>' + eventinfo.city +'<div>';
+            
+            var $eventImage = $("<img>");
+            //var eventImageURL = eventinfo.images.image[0].medium.url;
+            
+            var $resultsLink =  $("<a />", {
+                    //id : $event[i].id,
+                    //name : "link",
+                    href : '', //$event[i].title,
+                    text : 'Back to results',
+                    click : ( function(e) {e.preventDefault(); goToResults(); return false; } )
+                });
+
+
+
+            //console.log(eventImageURL);
+            //$eventImage.attr("src", eventImageURL);
+
+
+            var eventDate = '<div>'+ moment(eventinfo.start_time).format("dddd, MMMM Do YYYY, h:mm:ss a") +'<div>';
+            
+
+            console.log(eventinfo);
+
+            $('#event-details').append(eventTitle);
+            $('#event-details').append(eventDate);
+            $('#event-details').append(eventVenue);
+            $('#event-details').append(eventAddress);
+            $('#event-details').append(eventCity);
+            $('#event-details').append($resultsLink);
+            //$('#event-image').append($eventImage);
+
+            
+
+        });
+
+
+
+        //need API call here to get venue info outside of name
+        // var venueId = $event[i].venue_id;
+        // var vArgs = {
+        //     app_key: appKey,
+        //     id: venueId
+        // };
+        // EVDB.API.call("/venues/get", vArgs, function(oData) {
+        //     var venueinfo = oData;
+        //     var venueAddress = venueinfo.address;
+        //     var venueCity = venueinfo.city;
+        //     var venueState = venueinfo.region_abbr;
+        //     var venueCountry = venueinfo.country_abbr;
+        // });
+    }
+
+
     $("#search-events").on("click", function() {
 
         event.preventDefault();
+         $('#list-details').empty();
 
-        var appKey = "sS23z3239cvjhmN3"; //prasanth's app key for eventful
+        $('body').attr('id','results');
+
         var location = $("#location-request").val(); //currently our text box prompts for an address.   i believe address is not allowed, but rather we can pass in a combo of - city, state, country; zip code; venue ID; geocoordinates.  so we should probably fix our textbox to match this eventually
         var categories = "music"; //place for categories to search.  eventually will be pulling this from the categories list box.   multiple categories will have to be separated by commas
         var startDate = "20170301"; //place holder for start date.  eventually will be pulling tihs from the start date field.  we will also have to convert to the following format yyyymmdd00
@@ -63,48 +150,67 @@ $(document).ready(function() {
             for (var i = 0; i < $event.length; i++) {
                 console.log('Event Name  '+i+' '+ $event[i].title);
 
-                var eventName = '<h2>'+$event[i].title+'<h2>';
+                var $eventLink =  $("<a />", {
+                        id : $event[i].id,
+                        //name : "link",
+                        href : '', //$event[i].title,
+                        text : $event[i].title,
+                        click : ( function(e) {e.preventDefault(); getEventDetails(this.id); return false; } )
+                    });
+
+                var $eventName = $("<h2>");
+                $eventName.prepend($eventLink);
+
+
                 var eventVenue = '<div>'+$event[i].venue_name+'<div>';
                 var eventAddress = '<div>'+$event[i].venue_address+'<div>';
                 var eventCity = '<div>'+ $event[i].city_name +' '+ $event[i].region_name +', '+ $event[i].country_abbr+'<div>';
+                var eventDate = '<div>'+ moment($event[i].start_time).format("dddd, MMMM Do YYYY, h:mm:ss a") +'<div>';
+                //moment($event[i].venue_address).format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-                $('#list-details').append(eventName);
+                $('#list-details').append($eventName);
+                $('#list-details').append(eventDate);
                 $('#list-details').append(eventVenue);
                 $('#list-details').append(eventAddress);
                 $('#list-details').append(eventCity);
+                $('#list-details').append('<hr>');
+
+
+
+
 
                 //need this to call API and get event information
-                var eventId = $event[i].id;
-                var eArgs = {
-                    app_key: appKey,
-                    id: eventId
-                };
-                EVDB.API.call("/events/get", eArgs, function(oData) {
-                    var eventinfo = oData;
-                    var eventTitle = eventinfo.title;
-                });
+                // var eventId = $event[i].id;
+                // var eArgs = {
+                //     app_key: appKey,
+                //     id: eventId
+                // };
+                // EVDB.API.call("/events/get", eArgs, function(oData) {
+                //     var eventinfo = oData;
+                //     var eventTitle = eventinfo.title;
+                // });
 
-                var dateStartTime = $event[i].start_time;
-                var venueName = $event[i].venue_name;
+                // var dateStartTime = $event[i].start_time;
+                // var venueName = $event[i].venue_name;
 
-                //need API call here to get venue info outside of name
-                var venueId = $event[i].venue_id;
-                var vArgs = {
-                    app_key: appKey,
-                    id: venueId
-                };
-                EVDB.API.call("/venues/get", vArgs, function(oData) {
-                    var venueinfo = oData;
-                    var venueAddress = venueinfo.address;
-                    var venueCity = venueinfo.city;
-                    var venueState = venueinfo.region_abbr;
-                    var venueCountry = venueinfo.country_abbr;
-                });
+                // //need API call here to get venue info outside of name
+                // var venueId = $event[i].venue_id;
+                // var vArgs = {
+                //     app_key: appKey,
+                //     id: venueId
+                // };
+                // EVDB.API.call("/venues/get", vArgs, function(oData) {
+                //     var venueinfo = oData;
+                //     var venueAddress = venueinfo.address;
+                //     var venueCity = venueinfo.city;
+                //     var venueState = venueinfo.region_abbr;
+                //     var venueCountry = venueinfo.country_abbr;
+                // });
 
 
 
-                var eventUrl = $event[i].url;
-                var eventDesc = $event[i].description;
+                // var eventUrl = $event[i].url;
+                // var eventDesc = $event[i].description;
                 // var eventImage = $event[i].image.url;
                 // console.log(eventImage);
 
@@ -123,5 +229,6 @@ $(document).ready(function() {
 
 
     });
+
 
 });
