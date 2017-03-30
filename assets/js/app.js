724fa875ebd43c19fd800f740933044e3c0b6a9f
Prasanth Nannapaneni <prasanth.nannapaneni@northwestern.edu>
2017-03-30 17:30:41 -0500

    populating favorites div from firebase; only show description if its there; deleted bunch of old commented out code; when you click on Primer logo go to splash page; changed favorite places to favorite events

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,28 @@ $(document).ready(function() {
     //sS23z3239cvjhmN3 - key for eventful
 
     var appKey = "sS23z3239cvjhmN3"; //prasanth's app key for eventful
+    var nodeId = "";
+
+    var config = {
+    apiKey: "AIzaSyBwxOCXyFWRo8vRnYhHn5jRqVARLZuuueU",
+    authDomain: "primer-c6531.firebaseapp.com",
+    databaseURL: "https://primer-c6531.firebaseio.com",
+    storageBucket: "primer-c6531.appspot.com",
+    messagingSenderId: "735582248365"
+    };
+    
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database.
+    var database = firebase.database();
+
+    var starAscii = "&#9734;";
+    var lastObj = [];
+    var idList = [];
+    var lastKey;
+
+
+
 
     //This function sets the body id to "results" in order to display the results div 
     function goToResults(){
@@ -36,12 +58,23 @@ $(document).ready(function() {
         EVDB.API.call("/events/get", eArgs, function(oData) {
             var eventinfo = oData;
 
+
+
+            nodeId.set({
+               // set the results data for the current firebase node
+                eventinfo
+            }); // end nodeId.set block
+
+
+
             //Variables for the differentl data elements
             var eventTitle = '<h2>' + eventinfo.title +'<h2>';
             var eventAddress = '<div>' + eventinfo.address +'<div>';
             var eventVenue = '<div>' + eventinfo.venue_name +'<div>';
             var eventCity = '<div>' + eventinfo.city+', '+eventinfo.region + '<div>';
-            var eventDescription = '<div><h3>Description</h3>' + eventinfo.description +'<div>';
+            if(eventinfo.description){
+                var eventDescription = '<div><h3>Description</h3>' + eventinfo.description +'<div>';
+            }
             var eventURL = '<a href="'+eventinfo.url+'" target="_blank" class="more-info">'+ 'More info' +'<div>';
             
             //We'll probably end up not using the event image... TBD
@@ -64,12 +97,25 @@ $(document).ready(function() {
             //console.log(eventImageURL);
             //$eventImage.attr("src", eventImageURL);
 
+
+            // now append each search result to the results div, in the process, creating favorites buttons tagged with data attributes storing the related search key, search result number, and event ID
+            // var $eventFavorite = $("<button>");
+            // $eventFavorite.attr('type', 'buton');
+            // $eventFavorite.attr('data-key', lastKey);
+            // $eventFavorite.attr('data-num', 0);
+            // $eventFavorite.attr('data-eventID', eventinfo.id);
+            // $eventFavorite.attr('class', 'favs');
+            // $eventFavorite.text(starAscii);
+
+
+
             //Variable for the event date string
             var eventDate = '<div id="event-date">'+ moment(eventinfo.start_time).format("dddd, MMMM Do YYYY, h:mm a") +'<div>';
             
 
             console.log(eventinfo);
             //populates the event details - full event description
+            //$('#event-details').append($eventFavorite);
             $('#event-details').append($resultsLink);
             $('#event-details').append(eventTitle);
             $('#event-details').append(eventDate);
@@ -83,21 +129,89 @@ $(document).ready(function() {
             
 
         });
-
-        //need API call here to get venue info outside of name
-        // var venueId = $event[i].venue_id;
-        // var vArgs = {
-        //     app_key: appKey,
-        //     id: venueId
-        // };
-        // EVDB.API.call("/venues/get", vArgs, function(oData) {
-        //     var venueinfo = oData;
-        //     var venueAddress = venueinfo.address;
-        //     var venueCity = venueinfo.city;
-        //     var venueState = venueinfo.region_abbr;
-        //     var venueCountry = venueinfo.country_abbr;
-        // });
     }
+
+getDBData();
+function getDBData(){
+    // Firebase watcher + initial loader fires on any changes to a "value" in the database
+    database.ref().child('/searches').on("value", function(snapshot) {
+
+        // storing the snapshot.val() in a variable for convenience
+        var sv = snapshot.val();
+        
+        // Getting an array of each key in the snapshot object
+        var svArr = Object.keys(sv);
+        
+        // Finding the key for the last search
+        var lastIndex = svArr.length - 1;
+        lastKey = svArr[lastIndex];
+
+        console.log(svArr);
+        
+        // Using the key for the last search to access the last added search object
+        lastObj = sv[lastKey];
+
+                    
+        // this retrieves the favorites list and displays it in the favorites div
+        database.ref().child('/favEvents').once("value", function(snapshot) {
+            var favList = snapshot.val();
+            $("#favorites").html("");
+            // check to make sure that the /favEvents branch exists
+            if (favList) {        
+                var favlistArr = Object.keys(favList);
+                idList = [];
+              
+                // then loop through the list of search keys in the favorites list.  Each key here matches the node key for the related search in the /searches branch of the firebase database
+                for (var i=0; i < favlistArr.length; i++ ) {
+                    // retrieve the search results object corresponding to this search key
+                    var currObj = favList[favlistArr[i]];
+                    console.log(currObj);
+                    // retrieve the favorites object corresponding to this search key
+                    var numObj = favList[favlistArr[i]]; 
+                    // push the event ID to the idList array
+                    idList.push(favlistArr[i]);
+                    //if this eventID is already a favorite
+                    if (idList.indexOf(favlistArr[i]) > -1) {
+                        // set the star to filled
+                        starAscii = "&#9733;";
+                    } else { 
+                        // set the star to outlined
+                        starAscii = "&#9734;";
+                    } // end if/else// end if this eventID is already a favorite
+                    // now prepend each favorite event from the favorites list, including the star button to allow unfavoriting from the favorites div
+                    $("#favorites").prepend(currObj.title  + " at " + currObj.venue_name + " in " + currObj.city_name + "<br>Date & Time: "  + currObj.start_time + "<button type='button' data-key=" + currObj.search_key +  " data-num=" + currObj.result_num + " data-eventID=" + favlistArr[i] + " class='favs' style='background-color:white; border:none'>" + starAscii + "</button><br><br>");
+                } // end searches for loop
+            } // end if favList exists
+        }); // end favorites snapshot function 
+
+        // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    }); // end firebase watcher
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     $("#search-events").on("click", function() {
@@ -108,13 +222,23 @@ $(document).ready(function() {
 
         var location = $("#location-request").val(); //currently our text box prompts for an address.   i believe address is not allowed, but rather we can pass in a combo of - city, state, country; zip code; venue ID; geocoordinates.  so we should probably fix our textbox to match this eventually
         var categories = $("#category-select").val(); //place for categories to search.  eventually will be pulling this from the categories list box.   multiple categories will have to be separated by commas
-        var startDate = "20170301"; //place holder for start date.  eventually will be pulling tihs from the start date field.  we will also have to convert to the following format yyyymmdd00
-        var endDate = "20170401"; //place holder for end date.  eventually will be pulling tihs from the end date field.  we will also have to convert to the following format yyyymmdd00
-        // var dateConcatenated = startDate + "-" + endDate; //this is the format that date parameter will have to be put in to pass to api (yyyymmdd00-yyyymmdd-00)
-        var dateConcatenated = startDate;
+        
+        // var startDate = "20170301"; //place holder for start date.  eventually will be pulling tihs from the start date field.  we will also have to convert to the following format yyyymmdd00
+        // var endDate = "20170401"; //place holder for end date.  eventually will be pulling tihs from the end date field.  we will also have to convert to the following format yyyymmdd00
+
+        var startDate = moment($("#start-date").val()).format("YYYYMMDD") + "00";
+        var endDate = moment($("#end-date").val()).format("YYYYMMDD") + "00";
+       
+        //this is the format that date parameter will have to be put in to pass to api (yyyymmdd00-yyyymmdd-00)
+        var dateConcatenated = startDate + "-" + endDate;
         var radius = 5; //place holder for miles around address that we want to search.  eventually will be pulling from the miles around drop down box.
         var resultSize = 20; //place holder for results that you want to return from the web service.  eventually will be pulling from the results drop down box.
         
+
+
+        //Create an empty firebase node to store the search results
+        nodeId = firebase.database().ref().child('/searches').push();
+
 
         console.log('Search Location ' + location);
         console.log('Search Category ' + categories);
@@ -123,18 +247,6 @@ $(document).ready(function() {
         console.log('Number of results ' + resultSize);
         console.log('---------------------');
 
-
-        //this is the ideal ajax call, but not working so switching to use the call below instead
-        //var queryURL = "http://api.eventful.com/json/events/search?location=San+Diego&app_key=sS23z3239cvjhmN3&date=future&page_size=5";
-
-        // $.ajax({
-        //     url:  "http://api.eventful.com/json/events/search?location=San+Diego&app_key=sS23z3239cvjhmN3&date=future&page_size=5",
-        //     method: "GET"          
-        //   })
-        //   .done(function(response) {
-        //     //var results = response.data;
-        //     console.log('results');
-        //   });
 
         //here is the EVDB API call that does work.   the secret sauce to having this working is the inclusion of the script tag below in the index.html page
         //<script type="text/javascript" src="http://api.eventful.com/js/api"></script> -->secret sauce for EVDB.   won't work without including this above our app.js include
@@ -187,61 +299,7 @@ $(document).ready(function() {
                 $('#list-details').append(eventVenue);
                 $('#list-details').append(eventAddress);
                 $('#list-details').append(eventCity);
-
-
-
-
-
-                //need this to call API and get event information
-                // var eventId = $event[i].id;
-                // var eArgs = {
-                //     app_key: appKey,
-                //     id: eventId
-                // };
-                // EVDB.API.call("/events/get", eArgs, function(oData) {
-                //     var eventinfo = oData;
-                //     var eventTitle = eventinfo.title;
-                // });
-
-                // var dateStartTime = $event[i].start_time;
-                // var venueName = $event[i].venue_name;
-
-                // //need API call here to get venue info outside of name
-                // var venueId = $event[i].venue_id;
-                // var vArgs = {
-                //     app_key: appKey,
-                //     id: venueId
-                // };
-                // EVDB.API.call("/venues/get", vArgs, function(oData) {
-                //     var venueinfo = oData;
-                //     var venueAddress = venueinfo.address;
-                //     var venueCity = venueinfo.city;
-                //     var venueState = venueinfo.region_abbr;
-                //     var venueCountry = venueinfo.country_abbr;
-                // });
-
-
-
-                // var eventUrl = $event[i].url;
-                // var eventDesc = $event[i].description;
-                // var eventImage = $event[i].image.url;
-                // console.log(eventImage);
-
-                /*
-                      need to attach all the info gathered to the div tags.   can you guys take care of this in class today?
-                      also the title should probably be made to be a link passing the event ID somehow to the event detail page
-                      one other thing probably to eventually keep in mind after we get the information to post is to maybe push all
-                      event and venue information for a search into an array of objects.  this is so we dont have to keep calling the API.
-                      this is a much lower priority though i think
-                  */
             }
         });
-
-
-
-
-
     });
-
-
 });
